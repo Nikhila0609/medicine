382baad447bc0a0d82704dac74f390c8f97c4cae
Nikhila0609 <nikhitheprincess@gmail.com>
2024-07-11 17:58:00 +0530

    medicine added

---
+++ src/main/java/com/edubridge/medicineapp/App.java
@@ -0,0 +1,13 @@
+package com.edubridge.medicineapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
